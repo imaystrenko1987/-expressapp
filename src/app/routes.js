5e75e733ba213988a2d987be27319cf4887d60e0
Ivan Maistrenka <ivan_maistrenka@epam.com>
2019-02-08 18:15:35 +0300

    Password authentication

---
+++ src/app/routes.js
@@ -0,0 +1,129 @@
+const News = require('./models/news');
+
+module.exports = function(app, passport, logger) {
+	const getFullUrl = (req) => {
+		return `${req.method}: ${req.protocol}://${req.get('host')}${req.originalUrl}`;
+	};
+	
+	app.get('/', function(req, res) {
+		res.redirect('/news');
+	});
+
+	app.get('/login', function(req, res) {
+		res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+
+	app.post('/login', passport.authenticate('local-login', {
+		successRedirect : '/',
+		failureRedirect : '/login',
+		failureFlash : true
+	}));
+
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
+
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	app.get('/news', function (req, res) {
+		logger.info(getFullUrl(req));
+		News.find(function(err, articles) {
+			 if (err){
+			  res.send(err);
+		   }
+			 res.json(articles);
+		 });
+	 });
+	 
+	 app.get('/news/:id', function (req, res) {
+		logger.info(getFullUrl(req));
+		let id = req.params.id;
+		 News.findById(id, function(err, news) {
+			 if (err){
+			  res.send(err);
+		   } 
+			 res.json(news);
+		 });
+	 });
+	 
+	 app.post('/news', function (req, res) {
+		logger.info(getFullUrl(req));
+		logger.info(req.body.author);
+		isLoggedIn(req, res, function(){
+			News.create({
+			author: req.body.author,
+			title: req.body.title,
+			description: req.body.description,
+			content: req.body.content
+			}, function(err, articles) {
+				if (err){
+				res.send(err);
+			}
+		
+				News.find(function(err, articles) {
+					if (err){
+					res.send(err);
+				}
+					res.json(articles);
+				});
+			});
+		});
+	 });
+	 
+	 app.put('/news/:id', function (req, res) {
+		logger.info(getFullUrl(req));
+		let id = req.params.id;
+		isLoggedIn(req, res, function(){
+			var data = {
+			author: req.body.author,
+			title: req.body.title,
+			description: req.body.description,
+			content: req.body.content
+			}
+		
+			// save the user
+			News.findByIdAndUpdate(id, data, function(err, news) {
+			if (err) 
+			throw err;
+		
+			res.send('Successfully! Article updated - ' + news.title);
+			});
+		});
+	 });
+	 
+	 app.delete('/news/:id', function (req, res) {
+		logger.info(getFullUrl(req));
+		let id = req.params.id;
+		isLoggedIn(req, res, function(){
+			News.remove({
+				_id : id
+			}, function(err) {
+				if (err)
+					res.send(err);
+				else
+					res.send('Successfully! Article has been Deleted.');	
+			});	
+		});
+	 });
+	 
+	 app.get('*', function (req, res) {
+		logger.info(getFullUrl(req));
+		throw new Error('Page not found');
+	 });
+};
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+
+	res.redirect('/');
+}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,23 @@
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const winston = require('winston');
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const router = express.Router();
-const { createLogger, format, transports } = winston;
-const { combine, timestamp, label, printf } = format;
-var mongoose = require('mongoose');
-var database = require('./db/db');
+const port = process.env.PORT || 3336;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
+const configDB = require('./config/database.js');
+const winston = require('winston');
+const { format } = winston;
+const { combine, timestamp, printf } = format;
+require('./config/passport')(passport);
 
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-//app.use(methodOverride());
-
-//app.use(urlencodedParser);
-app.use('/',router);
-app.use(function(err,req,res,next) {
-  console.log(err.stack);
-  res.status(500);
-  res.render('pages/index', { error: err.message });
-});
-
-let server = app.listen(3336, function () {
-   let host = server.address().address
-   let port = server.address().port
-   
-   console.log("Listening at http://%s:%s", host, port)
-});
-
-//mongoose.connect('mongodb://localhost/news-db', { useNewUrlParser: true });
-var News = require('./db/models/news');
-mongoose.connect(database.url, database.options);
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.set('json spaces', 40);
+mongoose.connect(configDB.url, configDB.options);
 
 const myFormat = printf(info => {
   return `${info.timestamp} ${info.level}: ${info.message}`;
 });
-
-const getFullUrl = (req) => {
-	return `${req.method}: ${req.protocol}://${req.get('host')}${req.originalUrl}`;
-};
 
 const logger = winston.createLogger({
   format: combine(
@@ -58,87 +30,25 @@ const logger = winston.createLogger({
   ]
 });
 
-app.get('/', function (req, res) {
-	logger.info(getFullUrl(req));
-	res.redirect('/news');
-});
+app.use(bodyParser.urlencoded({'extended':'true'}));         
+app.use(bodyParser.json());                                     
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.set('view engine', 'ejs');
 
-app.get('/news', function (req, res) {
-   logger.info(getFullUrl(req));
-   News.find(function(err, articles) {
-		if (err){
-         res.send(err);
-      }
-		res.json(articles);
-	});
-});
+app.use(session({
+    secret: 'topsecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  }))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-app.get('/news/:id', function (req, res) {
-   logger.info(getFullUrl(req));
-   let id = req.params.id;
-	News.findById(id, function(err, news) {
-		if (err){
-         res.send(err);
-      } 
-		res.json(news);
-	});
-});
 
-app.post('/news', function (req, res) {
-   logger.info(getFullUrl(req));
-   logger.info(req.body.author);
-   News.create({
-      author: req.body.author,
-      title: req.body.title,
-      description: req.body.description,
-      content: req.body.content
-	}, function(err, articles) {
-		if (err){
-         res.send(err);
-      }
+require('./app/routes.js')(app, passport, logger);
 
-		News.find(function(err, articles) {
-			if (err){
-            res.send(err);
-         }
-			res.json(articles);
-		});
-	});
-});
-
-app.put('/news/:id', function (req, res) {
-   logger.info(getFullUrl(req));
-   let id = req.params.id;
-	var data = {
-      author: req.body.author,
-      title: req.body.title,
-      description: req.body.description,
-      content: req.body.content
-	}
- 
-	// save the user
-	News.findByIdAndUpdate(id, data, function(err, news) {
-   if (err) 
-      throw err;
- 
-	res.send('Successfully! Article updated - ' + news.title);
-	});
-});
-
-app.delete('/news/:id', function (req, res) {
-   logger.info(getFullUrl(req));
-   let id = req.params.id;
-	News.remove({
-		_id : id
-	}, function(err) {
-		if (err)
-			res.send(err);
-		else
-			res.send('Successfully! Article has been Deleted.');	
-	});
-});
-
-app.get('*', function (req, res) {
-   logger.info(getFullUrl(req));
-   throw new Error('Page not found');
-});
+let server = app.listen(port, function () {
+    console.log("Listening at http://%s:%s", server.address().address, port)
+ });
